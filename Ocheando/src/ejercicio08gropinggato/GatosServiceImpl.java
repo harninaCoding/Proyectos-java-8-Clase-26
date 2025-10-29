1307cb04cfe733871b26be0d37735e800f2818fe
programacion WEB <jasanchezm01@iesarroyoharnina.es>
2025-10-29 10:48:08 +0100

    Create GatosServiceImpl.java

---
+++ Ocheando/src/ejercicio08gropinggato/GatosServiceImpl.java
@@ -0,0 +1,32 @@
+package ejercicio08gropinggato;
+
+import java.util.Map;
+
+public class GatosServiceImpl implements GatosService {
+	GatosRepository gatosRepository;
+
+	@Override
+	public Map<Raza, Long> getCatsByRace() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getMostRepeatedName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Double getAgeAverage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Raza getMostNumerousRace() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
